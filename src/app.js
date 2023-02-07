bedaadc5b530cd319b75e720e6e07550b2070a40
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2023-02-07 17:45:03 +0530

    s

--- src/app.js
+++ src/app.js
@@ -41,9 +41,9 @@ exports.app = (server) => {
       }),
       cookie: {
         maxAge: 60 * 60 * 1000,
-        httpOnly: false,
-        sameSite: 'none',
-        secure: true,
+        // httpOnly: false,
+        // sameSite: 'none',
+        // secure: true,
         // domain: 'https://shauryaguleria.in',
       }, // 1 hour
     })
